b1444beea7df12063ff5b8b302b6c8d19745eee8
Vitória Moura <vim.saturnino@gmail.com>
2022-08-13 14:54:29 -0300

    criação das tabelas de customers e address

--- my_ecomerce.sql
+++ my_ecomerce.sql
@@ -1,3 +1,6 @@
+-- criando o banco de dados 
+CREATE DATABASE ecomerce;
+
 -- tabela de produtos
 CREATE TABLE products(
 	id int PRIMARY KEY,
@@ -6,6 +9,28 @@ CREATE TABLE products(
 	category varchar(40),
 	price numeric(10,2),
 	in_stock boolean
-	);
+);
 
 SELECT * FROM products;
+
+CREATE TABLE customers (
+	id int PRIMARY KEY,
+	name varchar(100) NOT NULL,
+	email varchar(60) NOT NULL UNIQUE,
+	cpf varchar(11) NOT NULL UNIQUE,
+	birthday date
+);
+
+SELECT * FROM customers;
+
+CREATE TABLE address (
+	id int PRIMARY KEY,
+	address varchar(100),
+	city varchar(100),
+	state varchar(2),
+	CONSTRAINT address_fk FOREIGN KEY (id) REFERENCES customers
+);
+
+SELECT * FROM address;
+
+SELECT * FROM customers c INNER JOIN address a on a.id = c.id;
