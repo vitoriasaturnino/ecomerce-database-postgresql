b96d9645ecb488502a7f7e4ae967fd5c9fcbbce9
Vitória Moura <vim.saturnino@gmail.com>
2022-08-15 10:20:09 -0300

    dados de inset para a tabela de orer items

---
+++ scripts/insert_order_item.sql
@@ -0,0 +1,26 @@
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 1, 1, 4);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 2, 6, 3);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 3, 10, 2);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 4, 11, 1);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 5, 15, 2);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 6, 5, 1);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 7, 3, 3);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 8, 11, 1);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 9, 2, 2);
+INSERT INTO order_items (order_id, product_id, quantity) VALUES ( 10, 12, 1);
+
+-- 'Computador'
+-- 'Celular'
+-- 'Ventilador'
+-- 'Televisor'
+-- 'Mouse'
+-- 'Bicicleta'
+-- 'Geladeira'
+-- 'Ar condicionado'
+-- 'Raquete de tênis'
+-- 'Sofá'
+-- 'Cama box'
+-- 'Mesa'
+-- 'Microfone'
+-- 'Mesa de tênis'
+-- 'Cafeteira'
